84f38b88a054ebfd29dda05bac1a35ab4af4d56f
corgi14 <xiangyegit@foxmail.com>
2024-04-03 19:17:01 +0800

    init

---
+++ db/migration/000001_init_schema.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS entries; --drop entries before accounts due to the references
+DROP TABLE IF EXISTS transfers; --drop transfers before accounts due to the references
+DROP TABLE IF EXISTS accounts;
+
